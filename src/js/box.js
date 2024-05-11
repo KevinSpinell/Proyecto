994fbe0ad274364438219c13b3df4a8dfb52d9f4
MaoJBN <mjbecerran@ufpso.edu.co>
2024-05-11 00:14:26 -0500

    Fix the box with select

--- src/js/box.js
+++ src/js/box.js
@@ -1,4 +1,5 @@
 import { saveAs } from 'file-saver';
+import jsPDF from 'jspdf';
 import Swal from 'sweetalert2';
     export { Swal };
 import 'sweetalert2/src/sweetalert2.scss'
@@ -7,6 +8,96 @@ const words = ["crisis","problemas","desarmonía","estrés", "mal", "solo","tris
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("submitButton").addEventListener("click", evaluartext);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("next").addEventListener("click", change);
+});
+
+function change() {
+    let selectElement = document.getElementById('select');
+    let selectElement1 = document.getElementById('select1');
+    selectElement.classList.remove('hidden');
+    selectElement1.classList.add('hidden');
+    
+}
+
+
+document.getElementById("option1").innerText="Cuentanos como es tu vida con tus familiares";
+document.getElementById("option2").innerText="Como te llevas con tus compañeros y si te gusta tu ambiente escolar";
+document.getElementById("option3").innerText="Que habitos de estudio practicas";
+document.getElementById("option4").innerText="Cuentanos acerca de tus fortalezas";
+document.getElementById("option5").innerText="Como es tu vida social en general";
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("option1").addEventListener("click", option1);
+});
+
+function option1() {
+    let selectElement = document.getElementById('select');
+    let selectElement1 = document.getElementById('Box');
+    selectElement1.classList.remove('hidden');
+    selectElement.classList.add('hidden');
+    let text = document.getElementById('text').innerText="como es tu vida con tus familiares"
+    let textarea = document.getElementById("text1");
+    textarea.focus();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("option2").addEventListener("click", option2);
+});
+
+function option2() {
+    let selectElement = document.getElementById('select');
+    let selectElement1 = document.getElementById('Box');
+    selectElement1.classList.remove('hidden');
+    selectElement.classList.add('hidden');
+    let text = document.getElementById('text').innerText="como te llevas con tus compañeros y si te gusta tu ambiente escolar"
+    let textarea = document.getElementById("text1");
+    textarea.focus();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("option3").addEventListener("click", option3);
+});
+
+function option3() {
+    let selectElement = document.getElementById('select');
+    let selectElement1 = document.getElementById('Box');
+    selectElement1.classList.remove('hidden');
+    selectElement.classList.add('hidden');
+    let text = document.getElementById('text').innerText="los habitos de estudio que practicas"
+    let textarea = document.getElementById("text1");
+    textarea.focus();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("option4").addEventListener("click", option4);
+});
+
+function option4() {
+    let selectElement = document.getElementById('select');
+    let selectElement1 = document.getElementById('Box');
+    selectElement1.classList.remove('hidden');
+    selectElement.classList.add('hidden');
+    let text = document.getElementById('text').innerText="como es tu vida social en general"
+    let textarea = document.getElementById("text1");
+    textarea.focus();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("option5").addEventListener("click", option5);
+});
+
+function option5() {
+    let selectElement = document.getElementById('select');
+    let selectElement1 = document.getElementById('Box');
+    selectElement1.classList.remove('hidden');
+    selectElement.classList.add('hidden');
+    let text = document.getElementById('text').innerText="como es tu vida con tus familiares"
+    let textarea = document.getElementById("text1");
+    textarea.focus();
+}
 
 function evaluartext() {
     let a = document.getElementById("text1").value
@@ -22,31 +113,20 @@ function evaluartext() {
     a=a.toLowerCase(a);
     let detect = a.split(" ");
     let SearchWords = [];
-
-    detect.forEach(detectWord => {
-        let WhioutPoint = detectWord.replace(/[!"'(),.:;?.]/ , "")
-        SearchWords.push(WhioutPoint)
-    });
-
-    let c=0;
-    console.log(SearchWords);
-    SearchWords.forEach(SearchWords => {
-        if (words.includes(SearchWords)) {
-            c++;
-        }
-    });
-    console.log(c);
     
-    // let textoUsuario = document.getElementById("text1").value.trim();
+    let textoUsuario = document.getElementById("text1").value.trim();
 
-    // const blob = new Blob([textoUsuario], { type: 'text/plain;charset=utf-8' });
 
+    const doc = new jsPDF();
+    // To Do arreglar como muestra el texto y el alert
+
+    doc.text(textoUsuario, 10, 10);
+
+
+    doc.save("archivo.pdf");
     
-    // saveAs(blob, 'datos/archivo.txt');
-
     localStorage.setItem("Texto",a)
 
     console.log(localStorage.getItem("Texto"));
     a = document.getElementById("text1").value=" "
 }
-
