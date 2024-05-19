0ec10d5c6f15c5898eeef295cd0c2a0445472f40
Bomberjoc <jrcoronelt@ufpso.edu.co>
2024-05-19 00:48:06 -0500

    Casi me saco un ojo poniendo gradientes

---
+++ src/components/vue/Questv.vue
@@ -0,0 +1,124 @@
+<script setup>
+    import {ref, onMounted} from 'vue'
+
+    let count = ref(0);
+
+
+    const countplus = () => {
+        if (count.value<17) {
+            count.value++;
+        }
+    }
+
+
+    // let quality = ref(["Muy poco","Poco","Más o menos","Un poco","Mucho"])
+    const bg_grad = ref([
+        "bg-gradient-to-r from-[#eeab2f] to-[#f2e9d8]"
+    ])
+    const gradientesArray = [
+  "bg-[#F2E9D8]", "bg-[#FDFFD4]", "bg-[#FCFAC9]", "bg-[#FBF4BE]", "bg-[#FAEFB3]",
+  "bg-[#F9EAA8]", "bg-[#F8E49D]", "bg-[#F7DE92]", "bg-[#F6D887]", "bg-[#F5D27C]",
+  "bg-[#F4CD71]", "bg-[#F3C766]", "bg-[#F2C25B]", "bg-[#F1BD50]", "bg-[#F0B745]",
+  "bg-[#EFB13A]", "bg-[#EEAB2F]"
+];
+
+
+   
+</script>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Julius+Sans+One&family=Kanit:wght@900&family=Poppins:ital,wght@0,400;0,500;0,700;0,800;1,600&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&display=swap');
+	
+	.julius-sans-one-regular {
+		font-family: "Julius Sans One", sans-serif;
+		font-weight: 400;
+        font-style: normal;
+	}
+	.archivo-narrow {
+		font-family: "Archivo Narrow", sans-serif;
+		font-optical-sizing: auto;
+		font-weight: <weight>;
+		font-style: normal;
+	}
+    .answer-div{
+        border: 2px solid rgb(187, 186, 186);
+        padding: 5px;
+        border-radius: 100%;
+        padding: 12px 20px;
+    }
+    .answer-div:hover{
+        cursor: pointer;
+    }
+    
+</style>
+<template>
+        <main class="h-lvh duration-100 ease-in" :class="gradientesArray[count]">
+            <div class="flex justify-center items-center pt-40 select-none">
+                <div class="bg-white w-[1000px] h-[600px] rounded-3xl shadow-2xl static">
+                    <div class="flex flex-col items-center justify-center h-full">
+                        <div id="back" class="absolute right-1/4 top-[20%] px-3 py-2 bg-[#804422] rounded-2xl text-white hover:cursor-pointer hover:bg-white hover:text-[#804422] border hover:border-[#804422] hover:scale-110 duration-150" onclick="window.location.href = 'index.html'">
+                            <p>
+                                Regresar
+                            </p>
+                        </div>
+                        <!-- <div id="help" class="absolute left-1/4 top-[20%] px-5 py-2 rounded-2xl border-2 border-[#804422] archivo-narrow text-base ">
+                            <p>
+                                5: Mucho | 4: Un poco | 3: Más o menos | 2: Poco | 1: Muy Poco
+                            </p>
+                        </div> -->
+                        <div class="w-2/5 h-[150px]  text-center flex items-center archivo-narrow text-3xl justify-center" id="questionid"></div>
+                        <div class="h-12  "></div>
+                        <div class="flex justify-around w-3/4 archivo-narrow" id="anscont">
+                            <div class="flex flex-col items-center gap-2">
+                                <div class="answer-div hover:scale-125 hover:bg-[#804422] duration-150 ease-in hover:text-white font-semibold" id="ans1" @click="countplus" >1</div>
+                                <div>Nada</div>
+                            </div>
+                            <div class="flex flex-col items-center gap-2">
+                                <div class="answer-div hover:scale-125 hover:bg-[#804422] duration-150 ease-in hover:text-white font-semibold" id="ans2" @click="countplus" >2</div>
+                                <div>Muy poco</div>
+                            </div>
+                            <div class="flex flex-col items-center gap-2">
+                                <div class="answer-div hover:scale-125 hover:bg-[#804422] duration-150 ease-in hover:text-white font-semibold" id="ans3" @click="countplus" >3</div>
+                                <div>Algo</div>
+                            </div>
+                            <div class="flex flex-col items-center gap-2">
+                                <div class="answer-div hover:scale-125 hover:bg-[#804422] duration-150 ease-in hover:text-white font-semibold" id="ans4" @click="countplus" >4</div>
+                                <div>Bastante</div>
+                            </div>
+                            <div class="flex flex-col items-center gap-2">
+                                <div class="answer-div hover:scale-125 hover:bg-[#804422] duration-150 ease-in hover:text-white font-semibold" id="ans5" @click="countplus" >5</div>
+                                <div>Mucho</div>
+                            </div>
+                        </div>
+                        <!-- <div class="flex justify-around w-3/4 border border-black"  >
+                            <div v-for="qty in quality">
+                                {{ qty }}
+                            </div>
+                        </div> -->
+                        <div id="sendQuest" class=" hidden flex-col items-center">
+                            <div class="text-3xl flex flex-col items-center ">
+                                <h3>Nos alegra que hayas respondido todas las preguntas</h3>
+                                <div class="h-[10px]"></div>
+                                <h3>Es hora de enviar las respuestas</h3>
+                            </div>
+                            <div class="h-[60px]"></div>
+                            <div class="inline-block">
+                                <button id="send" class="bg-[#804422] text-white py-2 px-3 rounded-3xl text-xl ">Enviar respuestas</button>
+                            </div>
+                        </div>
+                        <div id="mesDiv" class=" flex-col items-center px-32 leading-10">
+                            <div class="text-center">
+                                <h3 id="titleMessage" class="text-3xl julius-sans-one-regular"></h3>
+                                <div class="h-[30px]"></div>
+                                <div id="message" class="archivo-narrow text-lg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </main>
+    
+</template>
